e79bb06584460e162aecea712f5d7661aa0067b6
Siddharth <siddharth.muscat@gmail.com>
2021-08-16 14:28:36 +0530

    The refactoring incident

--- scripts/cmd-handler.js
+++ scripts/cmd-handler.js
@@ -7,6 +7,18 @@ class CommandHandler {
 	}
 
 	callCommand(text = '_default') {
+		const now = new Date();
+		if (now.getMonth() == 3 && now.getDate() == 1) {
+			if (window.pleaseActivateSuperSiddharthMode) {
+				term.writeln('Fine!');
+			} else {
+				this.term.writeln('sidsh: Unable to run command because Super Siddharth mode has not been activated')
+				this.term.writeln('It is being activated in the background');
+				this.term.writeln('\x1B[31;1mstop\x1B[0m the terminal to speed up')
+				return;
+			}
+		}
+
 		const commands = this.parseCommand(text);
 		commands.forEach(cmd => {
 			if (this.commands[cmd.main]) this.commands[cmd.main](term, cmd.args, this.cmdData);
@@ -26,8 +38,9 @@ class CommandHandler {
 			.flatMap(str => str.split(';'))
 			.flatMap(str => str.split('|'))
 			.map(str => {
+				if (str === '') return {main: '', args: ''};
 				const components = str.split(' ').map(str => str.trim()).filter(Boolean);
-				return {main: components.shift(), args: components.join(' ')}
+				return {main: components.shift(), args: components.join(' ')};
 			});
 	}
 }

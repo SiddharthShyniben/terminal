e39ecd9d4873f08cd25e68fb741df636db129ef3
Siddharth <siddharth.muscat@gmail.com>
2021-08-16 12:37:01 +0530

    More commands

--- scripts/commands.js
+++ scripts/commands.js
@@ -54,7 +54,10 @@ const commands = {
 			fs.writePath(arg, {})
 		}
 	},
-	shred: term => term.writeln('todo: maybe rickroll?'),
+	// Shred a file
+	shred: (_term, args) => {
+		fs.writePath(args, "We're no strangers to love\nYou know the rules and so do I\nA full commitment's what I'm thinking of\nYou wouldn't get this from any other guy\nI just wanna tell you how I'm feeling\nGotta make you understand\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nWe've known each other for so long\nYour heart's been aching but you're too shy to say it\nInside we both know what's been going on\nWe know the game and we're gonna play it\nAnd if you ask me how I'm feeling\nDon't tell me you're too blind to see\nNever gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you")
+	},
 	// Read a file and output
 	cat: (term, args) => {
 		if (args) {
